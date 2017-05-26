d6fe57032d3f9a4a294ab2946025f67486a15951
James Clarke <jamesraymondclarke@gmail.com>
2017-05-26 09:11:56 +0100

    working game, needs namespacing

---
+++ Project-1-READ.md
@@ -0,0 +1,48 @@
+#Anagram Falls 
+-
+#### Installation
+
+To run this code you will need to:
+
+- Download or clone the repo
+- In your command line `cd` into the repo then run`npm i` to install dependencies 
+- Then run `gulp` to compile the source code and open in browser
+
+> **Note**: You'll need to have `gulp-cli` installed globally
+> `npm i -g gulp-cli`
+
+-
+### The Project
+
+This project was to build a working game which invlolved a certain level of creativity and complexity.
+The game needed to involve a winning condition.
+
+The game I created was insipired by tetris and mixed with my liking of problem soloving, so using anagrams was a good way to merge the two.
+
+Here is a screenshot of the instructions of the game
+
+
+Here is a screenshot of the game in action
+
+-
+### Software and Languages 
+
+Here is a list of the software and languages used in the project:
+
+- HTML 5
+- SCSS 
+- JavaScript ES6
+- jQuery 3.10
+- Gulp
+- NPM
+- Git & GitHub
+
+-
+### Blockers and Obstacles
+
+An obstacle which was necessary to overcome was the validation of t
+
+
+
+
+
--- src/js/app.js
+++ src/js/app.js
@@ -14,7 +14,7 @@ $(() => {
     BBT: ['sheldon','leanord','spock','cheesecake factory','howard','star trek','penny','koothrapoli','bernadette','physics','laundry','halo night','dumplings', 'brisket','nasa','string theory','dark matter', 'comiccon','comic book'],
     LOTR: ['arragorn', 'gimli','gandalf','legolas','mordor','uruk hai','frodo','golumn','orcs','the shire','baggins','sauron', 'gondor','elves','hobbit','dwarves','two towers','black gate','faramir','saruman','shadowfax','witch king'],
     FT: ['arsenal','bournemouth','burnley','brighton','chelsea','crystal palace','everton','liverpool','newcastle','southampton','barcelona','juventus','ajax','bayern'],
-    CC: ['vienna','bridgetown','brasilia','brussels','sofia','hanoi','ottowa','bangui','washington','havana','prauge','cairo','helsinki','budapest','jakarta','jerusalem','kingston','tokyo','nairobi','beijing','rabat','oslo','pyongyang','madrid'],
+    CC: ['vienna','bridgetown','brasilia','brussels','sofia','hanoi','ottowa','bangui','washington','havana','prague','cairo','helsinki','budapest','jakarta','jerusalem','kingston','tokyo','nairobi','beijing','rabat','oslo','pyongyang','madrid'],
     WDI: ['function','javascript','switch','refactor','namespacing','jquery','const','primitive','object','array','responsive','constructor','recursive','indentation','iterate','method','algorithim','terminal','github','debugging','loop','variable'],
     SW: ['css']
   };
@@ -50,72 +50,71 @@ $(() => {
   $difficultyOptions.on('change', () => {
     selectedDifficulty = $difficultyOptions.val();
     difficulty = settings[selectedDifficulty];
-    console.log(difficulty);
   });
   ////////////////////Selectors End///////////////////////////
   ///////////////////Functions that change the word/////////////////////////
   /// This selects a random word from the selected array
   function generateWord() {
     ///This generates a random number within the length of the array, of the subject selected by the user (selectedSubject)and stores its index in a variable
-      const index = [Math.floor(Math.random() *
-   selectedSubject.length)];
+    const index = [Math.floor(Math.random() *
+      selectedSubject.length)];
 
-    /// This takes the selectedSubject and in the square brackets passes in the index variable so that the number genereated randomly from the length of the array now selects that word and stores it in selectedWord
+      /// This takes the selectedSubject and in the square brackets passes in the index variable so that the number genereated randomly from the length of the array now selects that word and stores it in selectedWord
     const selectedWord = selectedSubject[index];
 
-    /// This takes the selectedSubject and using splice gets rid of the word that was just previously stored in the selectedWord off the array by using its index
+      /// This takes the selectedSubject and using splice gets rid of the word that was just previously stored in the selectedWord off the array by using its index
     selectedSubject.splice(index, 1);
     return selectedWord;
   }
 
-  /// This takes the randomly selected word and shuffles it into an anagram
+    /// This takes the randomly selected word and shuffles it into an anagram
   function generateAnagram(selectedWord) {
-    /// This is splitting the selectedWord (a string) apart then sorting it a
+      /// This is splitting the selectedWord (a string) apart then sorting it a
     return selectedWord.split('').sort(function(){
       return 0.5 - Math.random();
     } ).join('');
   }
-  //////This generates a different element in a differnt position from the HTML
+    //////This generates a different element in a differnt position from the HTML
   function generateAnagramElement () {
+    ///creates a p tag with the class of anagram - stores it in a variable
     const $anagram = $('<p class="anagram"></p>');
+    // this generates a random number between the width of the of the game box, 250 is taken away so longer words do not overflow in the box and stores it in a varibale
     const left = Math.ceil((Math.random() * (width - 250)));
+    /// this sets the position of p-tag using the randomly genreated number as a psoition
     $anagram.css({left: left});
     return $anagram;
   }
-  ////// This is for when the word hits the bottom and 'falls off'
+    ////// This is for when the word hits the bottom and 'falls off'
   function anagramHitsTheBottom () {
     $('.anagram').each(function () {
       const position = Math.round($(this).position().top);
-      if (position >= height - 80) {
-        console.log(lifeScore);
-        if(!$(this).hasClass('fadeOutDown')) lifeScore -= 1;
+      if (position >= height - 75) {
+        if(!$(this).hasClass('hinge')) lifeScore -= 1;
         $lives.html(`Lives Left: ${lifeScore}`);//Not Working
         $(this).css('color','red');
-        $(this).addClass('animated fadeOutDown');
+        $(this).addClass('animated hinge');
         setTimeout(() => {
           $(this).remove();
         },500);
       }
     });
   }
-  ///////////////////Functions that change the word/////////////////////////
+    ///////////////////Functions that change the word/////////////////////////
 
-  //////////////////Main fucntion to play game ////////////////////////////
-  function playGame (){
-
-    console.log(width);
-    const selectedWord = generateWord();
-    const scrambledWord = generateAnagram(selectedWord);
-    const selectedAnagram = generateAnagramElement();
-    selectedAnagram.text(scrambledWord);
-    selectedAnagram.attr('data-word', selectedWord);
-    $container.append(selectedAnagram);
-    setInterval(function() {
-      selectedAnagram.css('top', '+=10px');
-      anagramHitsTheBottom();
-    },difficulty.speed);
-  }
-  ///////This is the reset function after a game has finished////////////////
+    //////////////////Main fucntion to play game ////////////////////////////
+    function playGame (){
+      const selectedWord = generateWord();
+      const scrambledWord = generateAnagram(selectedWord);
+      const selectedAnagram = generateAnagramElement();
+      selectedAnagram.text(scrambledWord);
+      selectedAnagram.attr('data-word', selectedWord);
+      $container.append(selectedAnagram);
+      setInterval(function() {
+        selectedAnagram.css('top', '+=10px');
+        anagramHitsTheBottom();
+      },difficulty.speed);
+    }
+    ///////This is the reset function after a game has finished////////////////
   function reset () {
     lifeScore = 5;
     $lives.html('Lives: 5');
@@ -123,7 +122,7 @@ $(() => {
     $winCondition.html('Correct Answers: 0');
     $('.anagram').remove();
   }
-  /// This is for when the player is ready to play, after the difficulty is selected and the play button is clicked - so this starts the game!
+    /// This is for when the player is ready to play, after the difficulty is selected and the play button is clicked - so this starts the game!
   $playGame.click( function(){
     if (selectedDifficulty === '' || selectedSubject === '') {
       alert('A Subject and Difficulty needs to be selected to proceed, please choose both...');
@@ -141,37 +140,36 @@ $(() => {
         } else{
           playGame();
         }
-      },difficulty.interval);
-    }});
-
-    /// This is the validation when the button is clicked to submit an answer, this also holds the animations applied to the game.
-    $('form').on('submit', function (e) {
-      e.preventDefault();
-      $('.anagram').each(function () {
-        const checker = $(this).data('word');
-        if ($('#answer').val().toLowerCase() === checker){
-          $(this).addClass('animated fadeOutUp').css('color','#39FF14');
-          $winCondition.html(`Correct Answers: ${winCondition = winCondition + 1}`);
-          setTimeout(() => {
-            $(this).remove();
-          },500);
-        } else {
-          $('.anagram').addClass('animated jello');
-          setTimeout(() => {
-            $('.anagram').removeClass('jello');
-          },500);
-        }
-      });
-      $('form').trigger('reset');
-    });
-
-    ///// this changes the height property based on the resizing of the browsers window
-    $(window).on('resize', () => {
-      height = $('.game-box').outerHeight(true);
-    });
-    $(window).on('resize', () => {
-      width = $('.game-box').width();
-    });
-
-    ////// end of the DOM;
+      },difficulty.interval); }
   });
+
+      /// This is the validation when the button is clicked to submit an answer, this also holds the animations applied to the game.
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+    $('.anagram').each(function () {
+      const checker = $(this).data('word');
+      if ($('#answer').val().toLowerCase() === checker){
+        $(this).addClass('animatedfadeOutUp').css('color','#39FF14');
+        $winCondition.html(`Correct Answers: ${winCondition =winCondition + 1}`);
+        setTimeout(() => {
+          $(this).remove();
+        },500);
+      } else {
+        $('.anagram').addClass('animated jello');
+        setTimeout(() => {
+          $('.anagram').removeClass('jello');
+        },500);
+      }
+    });
+    $('form').trigger('reset');
+  });
+
+      ///// this changes the height property based on the resizing of the browsers window
+  $(window).on('resize', () => {
+    height = $('.game-box').outerHeight(true);
+  });
+  $(window).on('resize', () => {
+    width = $('.game-box').width();
+  });
+
+});
